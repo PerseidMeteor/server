7dc48af3a8bcd58fcaf2a42dbd23e4a0356b7d94
breezeTuT <y_q_email@163.com>
2023-03-21 20:49:39 +0800

    添加blockqueue

--- log/log.h
+++ log/log.h
@@ -1,25 +1,84 @@
-/**
- * @file log.h
- * @author breezeTuT (y_q_email@163.com)
- * @brief
- * @date 2023-02-16
- *
- * @copyright Copyright (c) 2023
- *
- */
+/*
+ * @Author       : mark
+ * @Date         : 2020-06-16
+ * @copyleft Apache 2.0
+ */ 
+#ifndef LOG_H
+#define LOG_H
 
-class log
-{
-private:
-    /* data */
+#include <mutex>
+#include <string>
+#include <thread>
+#include <sys/time.h>
+#include <string.h>
+#include <stdarg.h>           // vastart va_end
+#include <assert.h>
+#include <sys/stat.h>         //mkdir
+#include <vector>
+#include "blockqueue.h"
+// #include "../buffer/buffer.h"
+
+class Log {
 public:
-    /// @brief
-    log(/* args */);
+    void init(int level, const char* path = "./log", 
+                const char* suffix =".log",
+                int maxQueueCapacity = 1024);
 
-    /// @brief
-    /// @return
-    int write();
+    static Log* Instance();
+    static void FlushLogThread();
 
-    /// @brief destrory object
-    ~log();
+    void write(int level, const char *format,...);
+    void flush();
+
+    int GetLevel();
+    void SetLevel(int level);
+    bool IsOpen() { return isOpen_; }
+    
+private:
+    Log();
+    void AppendLogLevelTitle_(int level);
+    virtual ~Log();
+    void AsyncWrite_();
+
+private:
+    static const int LOG_PATH_LEN = 256;
+    static const int LOG_NAME_LEN = 256;
+    static const int MAX_LINES = 50000;
+    static const int BUFFER_SIZE = 1024;
+
+    const char* path_;
+    const char* suffix_;
+
+    int MAX_LINES_;
+
+    int lineCount_;
+    int toDay_;
+
+    bool isOpen_;
+ 
+    vector<char> buff_;
+
+    int level_;
+    bool isAsync_;
+
+    FILE* fp_;
+    std::unique_ptr<BlockDeque<std::string>> deque_; 
+    std::unique_ptr<std::thread> writeThread_;
+    std::mutex mtx_;
 };
+
+#define LOG_BASE(level, format, ...) \
+    do {\
+        Log* log = Log::Instance();\
+        if (log->IsOpen() && log->GetLevel() <= level) {\
+            log->write(level, format, ##__VA_ARGS__); \
+            log->flush();\
+        }\
+    } while(0);
+
+#define LOG_DEBUG(format, ...) do {LOG_BASE(0, format, ##__VA_ARGS__)} while(0);
+#define LOG_INFO(format, ...) do {LOG_BASE(1, format, ##__VA_ARGS__)} while(0);
+#define LOG_WARN(format, ...) do {LOG_BASE(2, format, ##__VA_ARGS__)} while(0);
+#define LOG_ERROR(format, ...) do {LOG_BASE(3, format, ##__VA_ARGS__)} while(0);
+
+#endif //LOG_H
